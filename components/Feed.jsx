742e4bd9f44dca0e5330dfa9558d44b143f67c27
Muhammad Usama Shoaib <113609579+m-usamashoaib@users.noreply.github.com>
2024-01-30 13:12:51 +0500

    Added the Feed prompts

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import PromptCard from "./PromptCard";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [prompts, setPrompts] = useState([]);
+
+  const handleSearchChange = () => {};
+
+  useEffect(() => {
+    const fetchPrompts = async () => {
+      const response = await fetch("/api/prompt", { method: "GET" });
+      const data = await response.json();
+      setPrompts(data);
+    };
+    fetchPrompts();
+  }, []);
+
+  return (
+    <section className="feed">
+      <form>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PromptCardList data={prompts} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((prompt) => (
+        <PromptCard
+          key={prompt._id}
+          prompt={prompt}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Feed;
